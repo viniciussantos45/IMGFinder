1679ddce96c21cd5b96dbc1e21c77c860a632e8a
Vinicius Gomes <vinigomes.0311@gmail.com>
2020-04-05 18:55:05 -0300

    Version 0.2

--- IMGFinder/IMGFinder.py
+++ IMGFinder/IMGFinder.py
@@ -19,28 +19,22 @@ class IMGFinder:
 
         self.path = path
         self.imgs = os.listdir(self.path)
+        self.functions = dict()
 
-        
-        myFuctions = dict()
+        self.indexes = list()
 
-        for key in list(self.imgs):
-            myFuctions[key[:key.index('.')]] = ""
+        self.__indexing()
+        self.__getFunctions(functions)
 
-        for key in list(functions):
-            if key[0:2] != "__" and "<function" in str(functions[key]):
-                myFuctions[key] = functions[key]
-        self.functions = myFuctions
-
-    def monitorscreen(self, img):
+    def monitorScreen(self, img):
 
         # Chama a funcão de comparação de imagem e verifica se foi encontra ou não
         # Caso pos[0] seja = -1 siginifica que a imagem não foi encontrada
         # Realiza chamada de função pelo nome da imagem desejada
 
         if img[-5:] != '_.png':
-            pos = self.printscreen(self.path+img)
+            pos = self.printScreen(img)
             if pos[0] != -1:
-                print(img + " - econtrada na  posicao: ", pos[0], pos[1])
                 func = img.replace('.png', '')
                 
                 if "<function" in str(self.functions[func]):
@@ -48,12 +42,17 @@ class IMGFinder:
                 else:
                     warnings.warn(f"Function '{func}' not declared")
 
-                self.imgs.remove(img)
-            else:
-                print(img+" - nao encontrada na tela")
+                print(img + " - econtrada na  posicao: ", pos[0], pos[1])
+                #self.imgs.remove(img)
+                return True
 
-    @staticmethod
-    def printscreen(image, precision=0.8):
+            print(img+" - nao encontrada na tela")
+            return False
+
+
+    def printScreen(self, image, precision=0.8):
+        image = self.__concatNameWithIndex(image)
+        image = self.path+image
 
         # Método que compara imagem passada por parametro com a tela
         # tira um print da tela e compara com a imagem passada por parametro
@@ -71,17 +70,62 @@ class IMGFinder:
             return [-1, -1]
         return max_loc
 
-    def run(self):
+    def run(self, order = True):
         # Loop que busca a pasta de imagens e chama a função avaliaTela para verificar se encontrou a imagem
         # na tela
 
-        stop = False
-        while not stop:
-            if len(self.imgs) == 0:
-                stop = True
-            for img in self.imgs:
-                if img[-4:] == '.png':
-                    self.monitorscreen(img)
-            time.sleep(2)
-        
+        if not order:
+            stop = False
+            while not stop:
+                if len(self.imgs) == 0:
+                    stop = True
+                self.scrollsThroughImages()
+                time.sleep(2)
+        else:
+            self.scrollsThroughImages(True)
+                
+
         return print("Finish")
+
+
+    def scrollsThroughImages(self, utilFind = False):
+        for img in self.imgs:
+            if img[-4:] == '.png':
+                if utilFind:
+                    found = False
+                    ##Problema nesse while
+                    while not found:
+                        found = self.monitorScreen(img)
+                        
+                else:
+                    self.monitorScreen(img)
+            else:
+                print(f'Invalid file {img}')
+
+
+    def __indexing(self):
+        for indx in range(len(self.imgs)):
+            initialName = self.imgs[indx]
+            self.imgs[indx] = self.imgs[indx].replace(str(indx+1) + "-", "")
+            
+            arr = list()
+            arr.append(str(indx+1) + "-") if initialName != self.imgs[indx] else arr.append("") 
+            arr.append(self.imgs[indx])
+            self.indexes.append(arr)
+
+    
+    def __getFunctions(self, functions = object):
+        myFuctions = dict()
+
+        for key in list(self.imgs):
+            myFuctions[key[:key.index('.')]] = ""
+
+        for key in list(functions):
+            if key[0:2] != "__" and "<function" in str(functions[key]):
+                myFuctions[key] = functions[key]
+        self.functions = myFuctions
+
+    def __concatNameWithIndex(self, img):
+        for i in range(0, len(self.indexes)):
+            if self.indexes[i][1] == img:
+                return str(self.indexes[i][0] + self.indexes[i][1]) 
--- example.py
+++ example.py
@@ -3,7 +3,7 @@ import pyautogui
 import time
 
 
-def nameImage(par):
+def discoLocal(par):
     time.sleep(0.5)
     pyautogui.hotkey('win', 'r')
     time.sleep(0.5)
@@ -13,9 +13,18 @@ def nameImage(par):
     time.sleep(0.5)
     pyautogui.typewrite("FUNCIONOU")
 
+def cursos(par):
+    print("hahahahahh")
+
+def conexao(par):
+    print('Problema com tamanho de array')
+
+def compartilhar(par):
+    print("Passou por aqui também")
+
 
 #Directory of your images
-directoryImgs = 'C:\\Your\\Path\\Images\\'
+directoryImgs = '.\\Images\\'
 
 
 img = IMGFinder(directoryImgs, globals())
